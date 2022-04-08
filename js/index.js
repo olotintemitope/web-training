ecca95cb59a0659f35148de9fa697f3ade7c5833
Temitope Olotin <olotintemitope@gmail.com>
2022-04-08 15:50:17 +0100

    add javascript file

---
+++ js/index.js
@@ -0,0 +1,22 @@
+let phone = '07033235351';
+const a = 2;
+const b = 3;
+
+/**
+ * Integer
+ * Double | Float
+ * String
+ * Object
+ * Array
+ */
+
+
+phone = '0803344444444';
+phone = '0903344455433';
+
+const word = " The lazy dog jumps over the fence";
+const imgType = "profile.jpg";
+const plate1 = 'KJA-775-FF'
+
+
+console.log(word.trim());
