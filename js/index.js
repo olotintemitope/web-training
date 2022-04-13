6120ed17de7a44b8337fbe984c2bfb31c6413da0
Temitope Olotin <olotintemitope@gmail.com>
2022-04-13 14:45:31 +0100

    add array or object

--- js/index.js
+++ js/index.js
@@ -20,3 +20,47 @@ const plate1 = 'KJA-775-FF'
 
 
 console.log(word.trim());
+
+function Person() {
+
+}
+
+const person = new Person();
+
+person.name = 'Pelumi';
+person.age = 25;
+person.gender = 'Female';
+
+const person1 = {
+  name: 'Pelumi',
+  age: 25,
+  gender: 'Female'
+}
+
+const person2 = {
+  name: 'Adeayo',
+  age: 28,
+  gender: 'Male'
+}
+
+let people = [person1, person2];
+//people.push(person1);
+//people.push(person2)
+
+people.forEach(person => {
+ //console.table( people, ['name', 'age', 'gender']);
+});
+
+for (let i = 0; i < people.length; i++) {
+  //console.log(people[i].name, people[i].age, people[i].gender)
+}
+
+try {
+  console.log(people[2].name);
+  console.log(1/0);
+} catch (e) {
+  console.log(e+"".includes('TypeError:') ? 'Out Of Bound Exception' : e)
+}
+
+
+
